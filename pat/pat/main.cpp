a48d87ac201f4a1112d5e13797a217b82639f596
wei <1142529848@qq.com>
2019-09-26 15:26:31 +0800

    First Commit

---
+++ pat/pat/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pat
+//
+//  Created by 蒋为 on 2019/9/26.
+//  Copyright © 2019 蒋为. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
